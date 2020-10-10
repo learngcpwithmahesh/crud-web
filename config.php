23fc77f0ca5db0573ff34ea6f6b696edda2e5876
learngcpwithmahesh <64518661+learngcpwithmahesh@users.noreply.github.com>
2020-10-10 23:58:00 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $databaseHost = 'localhost';
-$databaseName = 'users';
-$databaseUsername = 'demo';
+$databaseName = 'users-web';
+$databaseUsername = 'users';
 $databasePassword = 'P@ssw0rd2020';
 
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
