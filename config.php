ac3dc4002f8c5e1ea2a956abea676b237fdcf7ce
learngcpwithmahesh <learngcpwithmahesh@gmail.com>
2020-10-10 15:39:45 +0000

    First commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$databaseHost = 'localhost';
+$databaseName = 'skills-sg';
+$databaseUsername = 'demo';
+$databasePassword = 'P@ssw0rd2020';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
